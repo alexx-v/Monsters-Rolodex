c69727a66b072ead53ceedbb98d129dc40189a52
alexx-v <aleksandr1vlk@gmail.com>
2020-05-05 19:16:32 +0300

    Updates

---
+++ precache-manifest.f0bdcbd6f72eb4bb751cf965b0312ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc7625c26a750b84d15cf8d640d3e64",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bf215396b696193202c6",
+    "url": "/monsters-rolodex/static/css/main.22b1b6b7.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf215396b696193202c6",
+    "url": "/monsters-rolodex/static/js/main.5c892444.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
